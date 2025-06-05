cc10235c2f73050a5033a063e1605f5a64bd357c
Mantu Sharma <169014870+mantusharma000@users.noreply.github.com>
2025-06-05 15:01:35 +0530

    Update 03_Reverse_Array.cpp

--- 03_Reverse_Array.cpp
+++ 03_Reverse_Array.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int arr[] = {10, 20, 30, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int temp[n];
+
+    for(int i = 0; i < n; i++) {
+        temp[i] = arr[n - i - 1];
+    }
+
+    // Copy back
+    for(int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+        cout << arr[i] << " ";
+    }
 }
+
